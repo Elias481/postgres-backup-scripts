c05ca10de3f6678c6b09530691dd001c21bfcccb
Elias Ohm <elias.ohm.de@gmail.com>
2026-04-08 02:10:45 +0200

    chore: remove legacy housekeeping.sh (replaced by backup_housekeeping.sh)

--- scripts/housekeeping.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "This script has been renamed to 'backup_housekeeping.sh'."
-echo "Please run './scripts/backup_housekeeping.sh' instead."
-echo "To preserve compatibility this wrapper will forward arguments now."
-exec "$(dirname "$0")/backup_housekeeping.sh" "$@"
